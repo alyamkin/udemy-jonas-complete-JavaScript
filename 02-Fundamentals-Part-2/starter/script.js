cbcc0267cdb0aa21802b053146ca2612a39cecff
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2020-12-22 08:55:21 -0500

    Add use strict mode lecture 32

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+//Activating Strict Mode 32
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;

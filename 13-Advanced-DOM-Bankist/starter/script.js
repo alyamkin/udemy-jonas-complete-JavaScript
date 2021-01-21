07bdefe1dd3842f3f70d42c92c57dcc191c6dafe
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2021-01-20 20:57:06 -0500

    Add lectures 185 - 188

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -9,6 +9,8 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const navLinks = document.querySelector('.nav__links');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,12 +34,30 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Smooth scrolling
+///////////////////////////////////////
+// Smooth scrolling "Learn more" button untill section1
 
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+///////////////////////////////////////
+// Page navigation
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // Matching strategy
+  if (
+    e.target.classList.contains('nav__link') &&
+    e.target.getAttribute('href') !== '#'
+  ) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
 //////////////////////////////////////////////////
 // 181. Selecting, Creating, and Deleting Elements
 
@@ -180,5 +200,94 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
 
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+*/
+
+/*
+// 184. Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function () {
+  alert('addEventListener: Great! You are reading the heading');
+  // remove event listener
+  //h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => {
+  // remove event listener
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
+
+// oldschool. Don't use
+// h1.onmouseenter = function () {
+//   alert('addEventListener: Great! You are reading the heading');
+// };
+*/
+
+/*
+// 185. Event Propagation: Bubbling and Capturing
+
+// rgb(255,255,255)
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randonColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randonColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.preventDefault();
+  this.style.cssText = `background-color:${randonColor()}`;
+  console.log('LINK', e.target, e.currentTarget);
+
+  //stop propogation
+  // e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.cssText = `background-color:${randonColor()}`;
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.cssText = `background-color:${randonColor()}`;
+  console.log('NAV', e.target, e.currentTarget);
+});
+*/
+
+/*
+// 188. DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+//Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)'; // closest finds closest parent of the element
+console.log(h1.closest('.header'));
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.transform = 'scale(0.5)';
+  }
 });
 */

b63a25a0e4e098ac15197bd8c8a63cfea94fb723
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2021-02-12 17:48:25 -0500

    Add lectures 200 - 207

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,63 @@
 'use strict';
+
+// 205. Constructor Functions and the new Operator
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create a method inside constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+
+console.log(jonas);
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function autpmatically return {}
+
+// const test = function () {
+//   console.log(this);
+// };
+
+// test();
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// 206. Prototypes
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+// .prototypeOfLinkedObjects
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas, matilda);
+
+console.log(jonas.hasOwnProperty('firstName'));
+
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(Number.isFinite(null));
+
+// 207. Prototypal Inheritance and The Prototype Chain
